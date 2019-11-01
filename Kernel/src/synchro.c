2da7d9e2534cf12b715b71f5dd576fda711ff3fd
Julian Neuss <julineuss@gmail.com>
2019-11-01 13:22:56 -0300

    a
    a

--- Kernel/src/synchro.c
+++ Kernel/src/synchro.c
@@ -10,7 +10,7 @@ typedef struct fd {
 // - se inicializan en 1, los identifica un numero que es el ID.
 // - se inicializan con una función tipo sem_init();
 // - si se hace wait el contador baja uno ( si no estaba en cero ) y si se hace post sube en uno.
-// - si se hace wait y el contador está en 0:
+// - antes del wait se verifica el contador, si esta en 0:
 //          1 - bloquear el proceso y ponerlo en la lista de bloqueados para ese semaforo.
 // - si se hace post y el semaforo estaba en 0:
-//          2 - desbloquear todos los procesos de la lista de bloqueados.
+//          2 - desbloquear un proceso de la lista de bloqueados de ese semaforo.
